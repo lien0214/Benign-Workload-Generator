cd1c017c35832adf343f79cf0569d34fa1c94cca
lien0214 <ted20030214@gmail.com>
2023-12-08 01:26:48 +0800

    pure_task.sh

--- benign-tools/workload-stress-ng/pure_task.sh
+++ benign-tools/workload-stress-ng/pure_task.sh
@@ -1,3 +1,5 @@
 stress-ng --matrix 5 --timeout 30s  &
 PID=$!
-sudo auditctl -a always,exit -F pid=$PID -F arch=b64 -S all -k test
+sudo auditctl -a always,exit -F arch=b64 -S all -k all
+sleep 120
+sudo auditctl -d always,exit -F arch=b64 -S all -k all
