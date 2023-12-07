3c5e23dff0e9597473afffbaa397c432dba8bbf7
lien0214 <ted20030214@gmail.com>
2023-12-08 02:11:07 +0800

    pure_task completed

--- benign-tools/workload-stress-ng/pure_task.sh
+++ benign-tools/workload-stress-ng/pure_task.sh
@@ -1,5 +1,23 @@
-stress-ng --matrix 5 --timeout 30s  &
-PID=$!
-sudo auditctl -a always,exit -F arch=b64 -S all -k all
-sleep 120
-sudo auditctl -d always,exit -F arch=b64 -S all -k all
+#!/bin/bash
+
+# Define the class names
+classes=(
+    "cpu" "io" "vm" "disk" "network" "cpu-cache" "device" "security"
+    "interrupt" "filesystem" "memory" "os" "pipe" "scheduler"
+)
+
+# Create or clear the log file
+echo "create log file"
+PID_log="PID.log"
+> "$PID_log"
+
+# Iterate over each class and run stress-ng
+for classname in "${classes[@]}"; do
+    stress-ng --class "$classname" --timeout 2m &
+    pid=$!
+    echo "stress-ng --class $classname: $pid" >> "$PID_log"
+    echo "stress-ng --class $classname: $pid"
+    wait $pid
+done
+
+echo "All stress tests completed."
