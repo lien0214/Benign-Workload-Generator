b2340b2344ada0191442f1e489144461de3fe16b
lien0214 <ted20030214@gmail.com>
2024-01-04 11:52:09 +0800

    to monitor2.0

--- benign-tools/workload-stress-ng/pure_task.sh
+++ benign-tools/workload-stress-ng/pure_task.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# clear /var/log/audit/ and start logging
-sudo rm -f /var/log/audit/*
 sudo auditctl -a always,exit -F arch=b64 -S all -k LALA
 
+# class
+# "cpu" "cpu-cache" "device" "io" "network" "interrupt" "filesystem" "memory"
+# "os" "pipe" "scheduler" "security" "vm"
+
 classes=(
-    "cpu" "io" "vm" "disk" "network" "cpu-cache" "device" "security"
-    "interrupt" "filesystem" "memory" "os" "pipe" "scheduler"
+    "pipe"
 )
 
 # Create and clear the log file
@@ -16,7 +17,7 @@ PID_log="PID.log"
 
 # run workload
 for classname in "${classes[@]}"; do
-    stress-ng --class "$classname" --seq 1 --timeout 2m >> "stress-ng-output.log" 2>&1 &
+    stress-ng --class "$classname" --seq 1 --timeout 2m --timestamp >> "stress-ng-output.log" 2>&1 &
     pid=$!
     echo "stress-ng --class $classname: $pid" >> "$PID_log"
     echo "stress-ng --class $classname: $pid"
@@ -27,3 +28,5 @@ echo "All stress tests completed."
 
 # end logging
 sudo auditctl -d always,exit -F arch=b64 -S all -k LALA
+sudo mv /var/log/audit/audit.log .
+# sudo cp /var/log/audit/audit.log log_pure_task
